4b7407d3e4247a0466afc0fae0abf1f58c79a28d
Nick Pezza <pezza@hey.com>
2024-09-10 11:16:27 -0400

    Rewrite development section if its set to use redis

--- lib/generators/solid_cable/install/install_generator.rb
+++ lib/generators/solid_cable/install/install_generator.rb
@@ -13,6 +13,12 @@ class SolidCable::InstallGenerator < Rails::Generators::Base
               new_production_cable_config)
   end
 
+  def configure_development_cable
+    gsub_file("config/cable.yml",
+              /development:\n\s*adapter: redis\n  url: .*\n/,
+              new_development_cable_config)
+  end
+
   private
 
   def new_production_cable_config
@@ -22,8 +28,23 @@ class SolidCable::InstallGenerator < Rails::Generators::Base
         connects_to:
           database:
             writing: cable
+            reading: cable
         polling_interval: 0.1.seconds
         keep_messages_around_for: 1.day
+    YAML
+  end
+
+  def new_development_cable_config
+    <<~YAML
+      development:
+        adapter: solid_cable
+        connects_to:
+          database:
+            writing: cable
+            reading: cable
+        polling_interval: 1.second
+        keep_messages_around_for: 1.day
+        silence_polling: true
     YAML
   end
 end
