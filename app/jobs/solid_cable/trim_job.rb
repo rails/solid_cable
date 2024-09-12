92adcd90422dd15d196625e704a597be76507352
Nick Pezza <pezza@hey.com>
2024-09-12 16:23:05 -0400

    Revert

--- app/jobs/solid_cable/trim_job.rb
+++ app/jobs/solid_cable/trim_job.rb
@@ -3,12 +3,12 @@
 module SolidCable
   class TrimJob < ActiveJob::Base
     def perform
-      trim_batches.times do
-        ::SolidCable::Message.transaction do
-          ids = ::SolidCable::Message.trimmable.non_blocking_lock.
-                limit(trim_batch_size).pluck(:id)
-          ::SolidCable::Message.where(id: ids).delete_all
-        end
+      return unless trim?
+
+      ::SolidCable::Message.transaction do
+        ids = ::SolidCable::Message.trimmable.non_blocking_lock.
+              limit(trim_batch_size).pluck(:id)
+        ::SolidCable::Message.where(id: ids).delete_all
       end
     end
 
@@ -18,13 +18,11 @@ module SolidCable
       ::SolidCable.trim_batch_size
     end
 
-    def trim_batches
+    def trim?
       expires_per_write =
         (1 / trim_batch_size.to_f) * ::SolidCable.trim_chance
-      batches = expires_per_write.floor
-      overflow_batch_chance = expires_per_write - batches
-      batches += 1 if rand < overflow_batch_chance
-      batches
+
+      rand < (expires_per_write - expires_per_write.floor)
     end
   end
 end
