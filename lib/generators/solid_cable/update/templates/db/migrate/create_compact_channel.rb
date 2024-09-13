49c89f4f18167d289f1fc1b1c24ca1c529645584
Nick Pezza <pezza@hey.com>
2024-09-12 22:48:27 -0400

    Remove null constraint when updating

--- lib/generators/solid_cable/update/templates/db/migrate/create_compact_channel.rb
+++ lib/generators/solid_cable/update/templates/db/migrate/create_compact_channel.rb
@@ -3,7 +3,7 @@
 class CreateCompactChannel < ActiveRecord::Migration[7.2]
   def change
     change_column :solid_cable_messages, :channel, :binary, limit: 1024, null: false
-    add_column :solid_cable_messages, :channel_hash, :integer, limit: 8, null: false, if_not_exists: true
+    add_column :solid_cable_messages, :channel_hash, :integer, limit: 8, if_not_exists: true
     add_index :solid_cable_messages, :channel_hash, if_not_exists: true
     change_column :solid_cable_messages, :payload, :binary, limit: 536_870_912, null: false
 
