029428ac8648712a4b4a0c5a14d415f12bc52347
Nick Pezza <pezza@hey.com>
2024-09-12 21:07:56 -0400

    Reset column info

--- bench/db/migrate/20240912235943_create_compact_channel.rb
+++ bench/db/migrate/20240912235943_create_compact_channel.rb
@@ -7,6 +7,7 @@ class CreateCompactChannel < ActiveRecord::Migration[7.2]
     add_index :solid_cable_messages, :channel_hash, if_not_exists: true
     change_column :solid_cable_messages, :payload, :binary, limit: 536_870_912, null: false
 
+    SolidCable::Message.reset_column_information
     SolidCable::Message.find_each do |msg|
       msg.update(channel_hash: SolidCable::Message.channel_hash_for(msg.channel))
     end
