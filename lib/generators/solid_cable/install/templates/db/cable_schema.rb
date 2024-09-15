90db2177d2a2958f6fed752c8436a2c86e8bd076
Bruno Prieto <bruno@37signals.com>
2024-09-15 15:52:03 -0300

    Remove updated_at column

--- lib/generators/solid_cable/install/templates/db/cable_schema.rb
+++ lib/generators/solid_cable/install/templates/db/cable_schema.rb
@@ -5,7 +5,6 @@ ActiveRecord::Schema[7.1].define(version: 1) do
     t.binary "channel", limit: 1024, null: false
     t.binary "payload", limit: 536870912, null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "channel_hash", limit: 8, null: false
     t.index ["channel"], name: "index_solid_cable_messages_on_channel"
     t.index ["channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
