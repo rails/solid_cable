91540ba9b65a32bd5db3484a026bbaedfcbe2c0b
Nick Pezza <pezza@hey.com>
2024-10-11 16:47:49 -0400

    Fix cop

--- lib/generators/solid_cable/add_channels/templates/db/migrate/create_channels.rb
+++ lib/generators/solid_cable/add_channels/templates/db/migrate/create_channels.rb
@@ -6,7 +6,7 @@ class CreateChannels < ActiveRecord::Migration[7.2]
       t.integer "channel_hash", limit: 8, null: false
       t.integer "subscribers", default: 0, null: false
       t.datetime "created_at", null: false
-      t.index ["channel_hash"], name: "index_solid_cable_channels_on_channel_hash"
+      t.index [ "channel_hash" ], name: "index_solid_cable_channels_on_channel_hash"
     end
   end
 end
