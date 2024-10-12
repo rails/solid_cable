cd8e0c931332cf4a48c523011ac80d8e299fbeec
Thiago Youssef <43591948+thiagoyoussef@users.noreply.github.com>
2024-10-12 10:07:14 -0300

    Fix irreversible upgrade migration (#44)

--- bench/db/migrate/20240912235943_create_compact_channel.rb
+++ bench/db/migrate/20240912235943_create_compact_channel.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CreateCompactChannel < ActiveRecord::Migration[7.2]
-  def change
+  def up
     change_column :solid_cable_messages, :channel, :binary, limit: 1024, null: false
     add_column :solid_cable_messages, :channel_hash, :integer, limit: 8, if_not_exists: true
     add_index :solid_cable_messages, :channel_hash, if_not_exists: true
@@ -11,5 +11,11 @@ class CreateCompactChannel < ActiveRecord::Migration[7.2]
     SolidCable::Message.find_each do |msg|
       msg.update(channel_hash: SolidCable::Message.channel_hash_for(msg.channel))
     end
+  end
+
+  def down
+    change_column :solid_cable_messages, :channel, :text
+    remove_column :solid_cable_messages, :channel_hash, if_exists: true
+    change_column :solid_cable_messages, :payload, :text
   end
 end
