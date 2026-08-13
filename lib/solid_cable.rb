10ab28a68822aa516f07ed277ca2633f033bd463
Nick Pezza <pezza@hey.com>
2026-08-12 20:24:50 -0400

    Memoize cable configuration (#86)

--- lib/solid_cable.rb
+++ lib/solid_cable.rb
@@ -2,70 +2,25 @@
 
 require "solid_cable/version"
 require "solid_cable/engine"
+require "solid_cable/configuration"
 require "action_cable/subscription_adapter/solid_cable"
 
 module SolidCable
   class << self
-    def connects_to
-      cable_config.connects_to.to_h.deep_transform_values(&:to_sym)
+    delegate :connects_to, :silence_polling?, :polling_interval,
+      :message_retention, :autotrim?, :trim_batch_size, :use_skip_locked,
+      :trim_chance, :reconnect_attempts, to: :configuration
+
+    def configuration
+      @configuration ||= Configuration.new(**Rails.application.config_for("cable"))
     end
 
-    def silence_polling?
-      cable_config.silence_polling != false
+    def configure(**options)
+      @configuration = Configuration.new(**options)
     end
 
-    def polling_interval
-      parse_duration(cable_config.polling_interval, default: 0.1.seconds)
+    def reset_configuration!
+      @configuration = nil
     end
-
-    def message_retention
-      parse_duration(cable_config.message_retention, default: 1.day)
-    end
-
-    def autotrim?
-      cable_config.autotrim != false
-    end
-
-    def trim_batch_size
-      if (size = cable_config.trim_batch_size.to_i) < 2
-        100
-      else
-        size
-      end
-    end
-
-    def use_skip_locked
-      cable_config.use_skip_locked != false
-    end
-
-    # For every write that we do, we attempt to delete trim_chance times as
-    # many records. This ensures there is downward pressure on the cache size
-    # while there is valid data to delete. Read this as 'every time the trim job
-    # runs theres a trim_multiplier chance this trims'. Adjust number to make it
-    # more or less likely to trim. Only works like this if trim_batch_size is
-    # 100
-    def trim_chance
-      2
-    end
-
-    def reconnect_attempts
-      attempts = cable_config.fetch(:reconnect_attempts, 1)
-      attempts = Array.new(attempts, 0) if attempts.is_a?(Integer)
-      attempts
-    end
-
-    private
-      def cable_config
-        Rails.application.config_for("cable")
-      end
-
-      def parse_duration(duration, default:)
-        if duration.present?
-          *amount, units = duration.to_s.split(".")
-          amount.join(".").to_f.public_send(units)
-        else
-          default
-        end
-      end
   end
 end
