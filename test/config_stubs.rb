59a7187aa1c5ba94abb266a3b8f6f3d351a8c396
Nick Pezza <pezza@hey.com>
2026-08-12 20:19:13 -0400

    Memoize cable configuration

--- test/config_stubs.rb
+++ test/config_stubs.rb
@@ -3,27 +3,9 @@
 module ConfigStubs
   extend ActiveSupport::Concern
 
-  class ConfigStub
-    def initialize(**)
-      @config = ActiveSupport::OrderedOptions.new.
-                update({ adapter: :test }.merge(**))
-    end
-
-    def config_for(_file)
-      @config
-    end
-
-    def executor
-      @executor ||= ExectorStub.new
-    end
-
-    class ExectorStub
-      def run!
-      end
-    end
-  end
-
   def with_cable_config(**)
-    Rails.stub(:application, ConfigStub.new(**)) { yield }
+    SolidCable.configure(**)
+    yield
+    SolidCable.reset_configuration!
   end
 end
