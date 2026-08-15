65f7ca65779782b73725dde4f181e1d45407fbd0
Nick Pezza <pezza@hey.com>
2026-08-15 10:49:16 -0400

    Batch writes together (#87)
    * Batch writes together
    
    * use sized queue
    
    * switch to sync when needed
    
    * be greedy
    
    * cleanup

--- lib/solid_cable.rb
+++ lib/solid_cable.rb
@@ -3,13 +3,15 @@
 require "solid_cable/version"
 require "solid_cable/engine"
 require "solid_cable/configuration"
+require "solid_cable/batched_broadcaster"
 require "action_cable/subscription_adapter/solid_cable"
 
 module SolidCable
   class << self
     delegate :connects_to, :silence_polling?, :polling_interval,
       :message_retention, :autotrim?, :trim_batch_size, :use_skip_locked,
-      :trim_chance, :reconnect_attempts, to: :configuration
+      :trim_chance, :reconnect_attempts, :writer_batch_size, :writer_batch_delay,
+      to: :configuration
 
     def configuration
       @configuration ||= Configuration.new(**Rails.application.config_for("cable"))
---
+++ lib/solid_cable/batched_broadcaster.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+module SolidCable
+  class BatchedBroadcaster
+    Stopped = Class.new(StandardError)
+    Message = Struct.new(:channel, :payload, keyword_init: true)
+
+    def initialize(batch_size: SolidCable.writer_batch_size, batch_delay: SolidCable.writer_batch_delay)
+      @batch_size = batch_size
+      @batch_delay = batch_delay
+      @queue = Queue.new
+
+      @thread = Thread.new do
+        Thread.current.name = "solid_cable_writer"
+        Thread.current.abort_on_exception = true
+        listen_for_initial_messages
+      end
+    end
+
+    def broadcast(channel, payload)
+      message = Message.new(channel:, payload:)
+
+      queue.enq message
+    rescue ClosedQueueError
+      raise Stopped, "Solid Cable writer has stopped"
+    end
+
+    def shutdown
+      queue.close
+      thread.join
+    end
+
+    private
+      attr_reader :batch_size, :batch_delay, :queue, :thread, :queue_size
+
+      def listen_for_initial_messages
+        loop do
+          message = queue.pop
+
+          break if message.nil?
+
+          collect_batch(message)
+        end
+      end
+
+      def collect_batch(first_message)
+        batch = [ first_message ]
+        deadline = monotonic_time + batch_delay
+
+        drain_queue_into(batch)
+        wait_for_messages_until(batch, deadline)
+
+        flush(batch)
+      end
+
+      def drain_queue_into(batch)
+        while batch.size < batch_size && (message = queue.pop(timeout: 0))
+          batch << message
+        end
+      end
+
+      def wait_for_messages_until(batch, deadline)
+        while batch.size < batch_size && (remaining = deadline - monotonic_time).positive?
+          message = queue.pop(timeout: remaining)
+          break if message.nil?
+
+          batch << message
+        end
+      end
+
+      def flush(batch)
+        Rails.application.executor.wrap do
+          SolidCable::Message.
+            broadcast_batch(batch.map { |message| [ message.channel, message.payload ] })
+        end
+      rescue StandardError => error
+        Rails.error.report(error)
+      end
+
+      def monotonic_time
+        Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      end
+  end
+end
